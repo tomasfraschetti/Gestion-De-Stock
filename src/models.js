a71d06158f2198894b1f77083eaf88b35a81e936
nicolas romero <nicoromerofrcu@gmail.com>
2026-05-05 09:09:59 -0300

    conexion fronend

--- src/models.js
+++ src/models.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose');
 
 const pedidoSchema = new mongoose.Schema({
-    cliente: { 
-        type: String, 
-        required: true 
-    },
-    total: { 
-        type: Number, 
-        required: true 
-    },
     fecha: {
         type: Date,
         default: Date.now // Se establecerá el momento exacto en el que llega o se crea
+    },
+    cliente: {
+        type: String,
+        required: true
     },
     items: [{
         nombre: { type: String, required: true },
         precio: { type: Number, required: true },
         cantidad: { type: Number, required: true, default: 1 }
-    }]
+    }],
+    total: {
+        type: Number,
+        required: true
+    },
 }, { timestamps: true }); // Mongoose además guarda 'createdAt' y 'updatedAt' automáticamente
 
 const Pedido = mongoose.model('Pedido', pedidoSchema);
